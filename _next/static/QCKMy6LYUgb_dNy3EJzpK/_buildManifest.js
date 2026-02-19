3c81372f86ea63c68e222131d745f03f5f6f03b0
malik-yeager <malik@runnerssoftware.com>
2026-02-19 15:05:26 +0530

    Updates

--- _next/static/QCKMy6LYUgb_dNy3EJzpK/_buildManifest.js
+++ _next/static/QCKMy6LYUgb_dNy3EJzpK/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/belanche//_next/:path+",
-        "destination": "/belanche/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
